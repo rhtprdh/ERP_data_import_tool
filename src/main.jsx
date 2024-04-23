336a1cf22829b7b8955b48c6297e524158e1c9ad
Rohit Pardhi <rhtprdh@gmail.com>
2024-04-23 21:51:12 +0530

    for checking

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,6 @@ import SearchableTable from './selectComponents/SearchableTable.jsx';
 import { Provider } from 'react-redux';
 import { store } from './app/Store.js';
 import CropYearMaster2 from './pages/masters/cropYearMaster/CropYearMaster2.jsx';
-// import QrTagWeightCapture from './pages/transactionComp/qrtagWeight/qrTagWeightCapture.jsx'fit
 import CbjExcel from './pages/excelMaster/CbjExcel.jsx';
 import EntityMaster from './pages/masters/EntityMaster/EntityMaster.jsx';
 import EntityMasterTwo from './pages/masters/EntityMaster/EntityMasterTwo.jsx';
@@ -36,8 +35,8 @@ import AddSplitter from './pages/excelMaster/AddSplitter.jsx';
 import SeriesSeqTwo from './pages/masters/seriesSeq/SeriesSeqTwo.jsx';
 import ProductionImport from './pages/excelMaster/ProductionImport.jsx';
 
-const editMode="true"
-let viewMode=true
+const editMode="true";
+let viewMode=true;
 
 const router = createBrowserRouter([
   {
@@ -56,133 +55,133 @@ const router = createBrowserRouter([
         path:"/tran",
         element: <Home dashboardType="tran"/>
       },
-      {
-        path:"/division-master",
-        element: <DivisionMaster1/>
-      },
-       {
-        path:"/division-master-page",
-        element: <DivisionMaster/>
-      },
-      {
-        path: "/division-master-page/:slug",
-        element: 
-          <DivisionMasterEdit/>  
-      },
+      // {
+      //   path:"/division-master",
+      //   element: <DivisionMaster1/>
+      // },
+      //  {
+      //   path:"/division-master-page",
+      //   element: <DivisionMaster/>
+      // },
+      // {
+      //   path: "/division-master-page/:slug",
+      //   element: 
+      //     <DivisionMasterEdit/>  
+      // },
       {
         path:"/entity-master",
         element: <EntityMaster/>
       },  
-      {
-        path:"/entity-master-page",
-        element: <EntityMasterTwo/>
-      },
-      {
-        path:"/entity-master-page/:slug",
-        element: <EntityMasterEdit/>
-      },
-      {
-        path:"/series-master",
-        element: <SeriesMaster/>
-      },  
-      {
-        path:"/series-master-page",
-        element: <SeriesMasterTwo/>
-      },
-      {
-        path:"/series-master-page/:slug",
-        element: <SeriesMasterEdit/>
-      },
-      {
-        path:"/stax-master",
-        element: <StaxMaster/>
-      },  
-      {
-        path:"/stax-master-page",
-        element: <StaxMasterTwo/>
-      },
-      {
-        path:"/stax-master-page/:slug",
-        element: <StaxMasterEdit/>
-      },
-      {
-        path:"/vrseq-master",
-        element: <SeriesSeq/>
-      },  
-      {
-        path:"/vrseq-master-page",
-        element: <SeriesSeqTwo/>
-      },
-      {
-        path:"/vrseq-master-page/:slug",
-        element: <SeriesSeqEdit/>
-      },
-
-      {
-        path: "/qr-tag",
-        element: 
-          <Qrtag/>  
-      },
-      {
-        path: "/qr-tag-print",
-        element: 
-          <QrTagPrint/>  
-      },
-      {
-        path: "/crop-year-master",
-        element: 
-          <CropYearMaster/>
-      },
-      {
-        path: "/crop-year-master-page",
-        element: 
-          <CropYearMaster2/>
-      },
-      {
-        path: "/search-table/:slug",
-        element: 
-        <SearchableTable />
-      },
       // {
-      //   path: "/qr-tag-weight/",
-      //   element: 
-      //   <QrTagWeightCapture/>
+      //   path:"/entity-master-page",
+      //   element: <EntityMasterTwo/>
       // },
-      {
-        path: "/cbj_excel/",
-        element: 
-        <CbjExcel/>
-      },
-      {
-        path: "/cbj_excel_daybook/",
-        element: 
-        <CbjExcelDaybook/>
-      },
-      {
-        path: "/cbj_excel_daybook/j",
-        element: 
-        <CbjExcelDaybookDrCrBackUp/>
-      },
-      {
-        path: "/indent/",
-        element: 
-        <IndentImport/>
-      },
-      {
-        path: "/order/",
-        element: 
-        <Order/>
-      },
-      {
-        path: "/add_splitter/",
-        element: 
-        <AddSplitter/>
-      },
-      {
-        path: "/prod/",
-        element: 
-        <ProductionImport/>
-      },
+      // {
+      //   path:"/entity-master-page/:slug",
+      //   element: <EntityMasterEdit/>
+      // },
+      // {
+      //   path:"/series-master",
+      //   element: <SeriesMaster/>
+      // },  
+      // {
+      //   path:"/series-master-page",
+      //   element: <SeriesMasterTwo/>
+      // },
+      // {
+      //   path:"/series-master-page/:slug",
+      //   element: <SeriesMasterEdit/>
+      // },
+      // {
+      //   path:"/stax-master",
+      //   element: <StaxMaster/>
+      // },  
+      // {
+      //   path:"/stax-master-page",
+      //   element: <StaxMasterTwo/>
+      // },
+      // {
+      //   path:"/stax-master-page/:slug",
+      //   element: <StaxMasterEdit/>
+      // },
+      // {
+      //   path:"/vrseq-master",
+      //   element: <SeriesSeq/>
+      // },  
+      // {
+      //   path:"/vrseq-master-page",
+      //   element: <SeriesSeqTwo/>
+      // },
+      // {
+      //   path:"/vrseq-master-page/:slug",
+      //   element: <SeriesSeqEdit/>
+      // },
+
+      // {
+      //   path: "/qr-tag",
+      //   element: 
+      //     <Qrtag/>  
+      // },
+      // {
+      //   path: "/qr-tag-print",
+      //   element: 
+      //     <QrTagPrint/>  
+      // },
+      // {
+      //   path: "/crop-year-master",
+      //   element: 
+      //     <CropYearMaster/>
+      // },
+      // {
+      //   path: "/crop-year-master-page",
+      //   element: 
+      //     <CropYearMaster2/>
+      // },
+      // {
+      //   path: "/search-table/:slug",
+      //   element: 
+      //   <SearchableTable />
+      // },
+      // // {
+      // //   path: "/qr-tag-weight/",
+      // //   element: 
+      // //   <QrTagWeightCapture/>
+      // // },
+      // {
+      //   path: "/cbj_excel/",
+      //   element: 
+      //   <CbjExcel/>
+      // },
+      // {
+      //   path: "/cbj_excel_daybook/",
+      //   element: 
+      //   <CbjExcelDaybook/>
+      // },
+      // {
+      //   path: "/cbj_excel_daybook/j",
+      //   element: 
+      //   <CbjExcelDaybookDrCrBackUp/>
+      // },
+      // {
+      //   path: "/indent/",
+      //   element: 
+      //   <IndentImport/>
+      // },
+      // {
+      //   path: "/order/",
+      //   element: 
+      //   <Order/>
+      // },
+      // {
+      //   path: "/add_splitter/",
+      //   element: 
+      //   <AddSplitter/>
+      // },
+      // {
+      //   path: "/prod/",
+      //   element: 
+      //   <ProductionImport/>
+      // },
     ]
   }
 ])
@@ -193,4 +192,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <RouterProvider router={router}/>,
   {/* </React.StrictMode>, */}
   </Provider>
-)
+);
