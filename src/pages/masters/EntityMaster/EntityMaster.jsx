9e25f888fe009d2d526dad08509a6552fec6d963
rhtprdh <rohit.pardhi96@gmail.com>
2024-04-29 23:08:52 +0530

    entity user change

--- src/pages/masters/EntityMaster/EntityMaster.jsx
+++ src/pages/masters/EntityMaster/EntityMaster.jsx
@@ -15,7 +15,7 @@ function EntityMaster() {
 
        useEffect(() => {
         // console.log( axios.get('/api/division'))
-        axios.get('13.126.114.161:3000/api/entity')
+        axios.get('//13.126.114.161:3000/api/entity')
         .then((response) => {
           setRows(response.data.data)
           
